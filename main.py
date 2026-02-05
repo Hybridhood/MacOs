d3c8cf43a55d5cb5268e1af3551018ec5b2df830
Benjamin Hogus <67559008+Hybridhood@users.noreply.github.com>
2026-02-05 19:10:27 +0000

    new file:   .vscode/launch.json 	new file:   app.py 	modified:   main.py 	modified:   static/css/styles.css 	new file:   static/js/plot.js 	modified:   templates/about.html 	modified:   templates/index.html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import yfinance as yf
 
 app = Flask(__name__)
 # this is beautiful - amir
@@ -7,6 +8,12 @@ def home():
     return render_template("index.html")
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    apple = yf.Ticker("AAPL")           # Apple’s stock symbol
+    info = apple.info                   # Company major details
+    hist = apple.history(period="1y")
+    dates = hist["Close"].tolist()  # Closing prices for the last year
+
+    
+    return render_template("about.html", dates=dates, hist=hist)
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
