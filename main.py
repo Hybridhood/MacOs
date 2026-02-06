9d50e6827a5cb5b7039089f4f26dda68ad32834b
Benjamin Hogus <67559008+Hybridhood@users.noreply.github.com>
2026-02-06 04:14:19 +0000

    deleted:    app.py 	modified:   main.py 	modified:   static/js/plot.js

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def home():
     return render_template("index.html")
 @app.route("/about")
 def about():
-    apple = yf.Ticker("AAPL")           # Apple’s stock symbol
-    info = apple.info                   # Company major details
+    apple = yf.Ticker("AAPL")           # Apple’s stock symbol                 # Company major details
     hist = apple.history(period="1y")
-    dates = hist["Close"].tolist()  # Closing prices for the last year
-
+    dates = hist.index.strftime('%m/%d/%Y').tolist()
+ # Slice the list to get elements from index 1 to 3 (exclusive)
+    hist = hist['Close'].tolist()  # Closing prices for the last year
+      # Dates for the last year
+    print(dates)
+    print(hist)
     
     return render_template("about.html", dates=dates, hist=hist)
 if __name__ == "__main__":
--- static/js/plot.js
+++ static/js/plot.js
@@ -1,6 +1,6 @@
-const xArray = [50,60,70,80,90,100,110,120,130,140,150];
-const yArray = [7,8,8,9,9,9,10,11,14,14,15];
-print(Stockdata.info, Stockdata.hist);
+const xArray = stockData.x;
+const yArray = stockData.y;
+alert(xArray);
 // Define Data
 const data = [{
   x: xArray,
@@ -11,8 +11,8 @@ const data = [{
 
 // Define Layout
 const layout = {
-  xaxis: {range: [40, 160], title: "Square Meters"},
-  yaxis: {range: [5, 16], title: "Price in Millions"},
+  xaxis: {range: [1, 400], title: "Square Meters"},
+  yaxis: {range: [1, 400], title: "Price in Millions"},
   title: "House Prices vs Size"
 };
 
